382177fc1af623876497da7a2e5a43f2e79e2d78
William Bruno <williambrunosalesdepaulalima@gmail.com>
2021-07-20 22:07:24 -0300

    understading about css on react

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { React } from 'react';
 import Results from './Components/Results';
+
 export default function App(props) {
   const sum =(n1, n2) => {
     return n1 + n2;
--- src/Components/Results.js
+++ src/Components/Results.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../Styles/Results.css';
 
 export default function Results(props) {
   const n1 = props.n1;
